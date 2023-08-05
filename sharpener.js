64e4a144d39ff454e6cb2ce466c53c6a15cc1308
MVinesh4886 <134615779+MVinesh4886@users.noreply.github.com>
2023-08-05 23:26:50 +0530

    Update1 sharpener.js

--- sharpener.js
+++ sharpener.js
@@ -58,3 +58,70 @@ var nthChild = document.querySelectorAll("li:nth-child(2)");
 for (var i = 0; i < odd.length; i++) {
   odd[i].style.backgroundColor = "green";
   even[i].style.color = "green";
+}
+//Traversing The DOm
+var itemList = document.querySelector("#items");
+//Parent Node
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNodes
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = "yellow";
+
+// //FirstChild
+// console.log(itemList.firstChild);
+// //FirstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = "Hello";
+
+//lastChild
+// console.log(itemList.lastChild);
+// //lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = "Hello 4";
+
+//nextSibling
+// console.log(itemList.nextSibling);
+//nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+//previousSibling
+// console.log(itemList.previousSibling);
+//previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementChild.style.color = "green";
+
+//createElement
+
+//create a div
+var newDiv = document.createElement("div");
+
+//Add class
+newDiv.className = "hello";
+//Add id
+newDiv.id = "hello1";
+//Add attributes
+newDiv.setAttribute("title", "Hello Div");
+
+//create text node
+var newDivText = document.createTextNode("Hello world");
+//Add text to Div
+newDiv.appendChild(newDivText);
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+
+console.log(newDiv);
+newDiv.style.fontSize = "20px";
+container.insertBefore(newDiv, h1);
+
+
